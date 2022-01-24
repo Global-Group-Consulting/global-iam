9f7c62b297b24d341bf9242f7dbacc85bfc6fc59
leicaflorian <florian.leica@gmail.com>
2022-01-24 22:00:05 +0100

    Adds Debugbar and DB configuration

--- config/database.php
+++ config/database.php
@@ -34,77 +34,73 @@ return [
     */
 
     'connections' => [
-
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
-
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-        ],
-        
-        'mongodb' => [
-          'driver' => 'mongodb',
-          // 'host' => env('DB_HOST', '127.0.0.1'),
-          // 'port' => env('DB_PORT', 27017),
-          'database' => env('DB_DATABASE', 'homestead'),
-          // 'username' => env('DB_USERNAME', 'homestead'),
-          // 'password' => env('DB_PASSWORD', 'secret'),
-          'dsn' => env('DB_DSN'),
-          'options' => [
-              // here you can pass more settings to the Mongo Driver Manager
-              // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
-      
-              'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
-          ],
+  
+      'sqlite' => [
+        'driver'                  => 'sqlite',
+        'url'                     => env('DATABASE_URL'),
+        'database'                => env('DB_DATABASE', database_path('database.sqlite')),
+        'prefix'                  => '',
+        'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+      ],
+  
+      'mysql' => [
+        'driver'         => 'mysql',
+        'url'            => env('DATABASE_URL'),
+        'host'           => env('DB_HOST', '127.0.0.1'),
+        'port'           => env('DB_PORT', '3306'),
+        'database'       => env('DB_DATABASE', 'forge'),
+        'username'       => env('DB_USERNAME', 'forge'),
+        'password'       => env('DB_PASSWORD', ''),
+        'unix_socket'    => env('DB_SOCKET', ''),
+        'charset'        => 'utf8mb4',
+        'collation'      => 'utf8mb4_unicode_ci',
+        'prefix'         => '',
+        'prefix_indexes' => true,
+        'strict'         => true,
+        'engine'         => null,
+        'options'        => extension_loaded('pdo_mysql') ? array_filter([
+          PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        ]) : [],
+      ],
+  
+      'pgsql' => [
+        'driver'         => 'pgsql',
+        'url'            => env('DATABASE_URL'),
+        'host'           => env('DB_HOST', '127.0.0.1'),
+        'port'           => env('DB_PORT', '5432'),
+        'database'       => env('DB_DATABASE', 'forge'),
+        'username'       => env('DB_USERNAME', 'forge'),
+        'password'       => env('DB_PASSWORD', ''),
+        'charset'        => 'utf8',
+        'prefix'         => '',
+        'prefix_indexes' => true,
+        'schema'         => 'public',
+        'sslmode'        => 'prefer',
+      ],
+  
+      'sqlsrv' => [
+        'driver'         => 'sqlsrv',
+        'url'            => env('DATABASE_URL'),
+        'host'           => env('DB_HOST', 'localhost'),
+        'port'           => env('DB_PORT', '1433'),
+        'database'       => env('DB_DATABASE', 'forge'),
+        'username'       => env('DB_USERNAME', 'forge'),
+        'password'       => env('DB_PASSWORD', ''),
+        'charset'        => 'utf8',
+        'prefix'         => '',
+        'prefix_indexes' => true,
+      ],
+  
+      'mongodb' => [
+        'driver'   => 'mongodb',
+        'database' => env('DB_DATABASE'),
+        'dsn'      => env('DB_DSN'),
+      ],
+  
+      'mongodb_legacy' => [
+        'driver'   => 'mongodb',
+        'database' => env('DB_DATABASE_LEGACY'),
+        'dsn'      => env('DB_DSN_LEGACY'),
       ],
     ],
 
